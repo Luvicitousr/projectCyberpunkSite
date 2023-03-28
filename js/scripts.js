46b54a6cc1e6c4aff6bb6a3072c22403efdcadde
Luvicitousr <luvicitousr@gmail.com>
2023-03-28 19:30:31 -0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
-var glitched = document.querySelector('.glitched');
+const h1glitchedbefore = document.querySelector('h1.cyberpunk.glitched');
 var on = false;
+var ms = 3000;
 
 var update = function(){
-    glitched.setAttribute('animation-name', (on) ? 'glitched' : '');
-    console.log("entrou aqui")
+    if(on){
+        $("p.cyberpunk.glitched").css("animation-name", "glitched");
+        $("h1.cyberpunk.glitched").css("animation-name", "glitched");
+        ms = 900;
+    }else{
+        $("p.cyberpunk.glitched").css("animation-name", "nothing");
+        $("h1.cyberpunk.glitched").css("animation-name", "nothing");
+        ms = 3000;
+    }
+
+    h1glitchedbefore.style.setProperty('--glitched-animation', (on) ? 'beforeglitched' : 'nothing');
+
     on = !on;
     setTimer();
+    
 }
 
 var setTimer = function(){
     setTimeout(function(){
         requestAnimationFrame(update);
-    }, 3000);
+    }, ms);
 };
 
 setTimer();
